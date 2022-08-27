4de28462d0839a228ce5433be83ed57909ff8fd1
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2022-08-27 07:47:04 +0700

    Implement getAll foods API

--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -1,10 +1,45 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
+// const pick = require('../utils/pick');
 const catchAsync = require('../utils/catchAsync');
-const { Food, FoodCategory, FoodAndFoodCategory, Location, FoodAndLocation } = require('../models');
+const { Food, FoodCategory, Location, Restaurant } = require('../models');
 
 const getAll = catchAsync(async (req, res) => {
-  const { page = 1, limit = 10, search = '', locationId = false } = req.query;
+  const {
+    // page = 1,
+    // limit = 10,
+    // search = '',
+    locationId = false,
+    category = false,
+    restaurantId = false,
+  } = req.query;
+
+  if (locationId) {
+    await Location.findById(locationId)
+      .populate('foods')
+      .then((foods) => {
+        res.status(httpStatus.OK).send({ foods: foods.foods });
+      });
+  }
+
+  if (category) {
+    await FoodCategory.find({ name: category })
+      .populate('foods')
+      .then((foods) => {
+        res.status(httpStatus.OK).send({ foods: foods.foods });
+      });
+  }
+
+  if (restaurantId) {
+    await Restaurant.findById(restaurantId)
+      .populate('foods')
+      .then((foods) => {
+        res.status(httpStatus.OK).send({ foods: foods.foods });
+      });
+  }
+
+  await Food.Food.find().then((foods) => {
+    res.status(httpStatus.OK).send({ foods });
+  });
 });
 
 module.exports = {
